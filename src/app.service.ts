7dc0f6e7541b04e4315b9df1cd01b8428acaf981
Mateus Santos <mateuspele2015@gmail.com>
2025-04-07 01:16:03 -0300

    features: added new features

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): object {
+  getHello() {
     return {
       message: 'API do sistema de gestão de verificação de veículos',
       version: '1.0',
